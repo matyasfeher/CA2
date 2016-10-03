f063ac0d8c393617b407eaa97d4267c8253ebb8d
matyasfeher <fehermatyas17@gmail.com>
2016-10-03 15:32:13 +0200

    Created the facade's interface Started to work on the facade Changed AUTO->IDENTITY created getters setters and constructors

--- CA2/src/main/java/entity/CityInfo.java
+++ CA2/src/main/java/entity/CityInfo.java
@@ -15,7 +15,7 @@ import javax.persistence.Id;
 public class CityInfo implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     
     private int zipCode;
--- CA2/src/main/java/entity/Person.java
+++ CA2/src/main/java/entity/Person.java
@@ -14,7 +14,7 @@ import javax.persistence.Id;
 public class Person implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     
     private String firstName;
