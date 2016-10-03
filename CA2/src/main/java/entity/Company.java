10cac1a5d99ddaf4a2c6492ddf3bfe65819efc81
enny25 <fetahond@gmail.com>
2016-10-03 15:33:56 +0200

    added relationships to entities

--- CA2/src/main/java/entity/Company.java
+++ CA2/src/main/java/entity/Company.java
@@ -14,7 +14,7 @@ import javax.persistence.Id;
 public class Company extends InfoEntity {
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     
     private String name;
