8aed381eedeef5ec34235c408da5d9b3b11544d2
enny25 <fetahond@gmail.com>
2016-10-03 15:24:45 +0200

    added inheritance

--- CA2/src/main/java/entity/Company.java
+++ CA2/src/main/java/entity/Company.java
@@ -11,7 +11,7 @@ import javax.persistence.Id;
  * @author Acer
  */
 @Entity
-public class Company implements Serializable {
+public class Company extends InfoEntity {
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
