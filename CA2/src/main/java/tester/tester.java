d02a5a127706b932f72fd72d9774af4279920d56
Zygi <Zygi@10.50.138.131>
2016-10-05 15:01:18 +0200

    Fixed dependecies

--- CA2/src/main/java/tester/tester.java
+++ CA2/src/main/java/tester/tester.java
@@ -1,24 +1,50 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package tester;
 
-import facade.TheFacade;
+import entity.Address;
+import entity.CityInfo;
+import entity.Company;
+import entity.Hobby;
+import entity.InfoEntity;
+import entity.Person;
+import entity.Phone;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 /**
  *
- * @author Acer
+ * @author Zygi
  */
 public class tester {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        Persistence.generateSchema("PU", null);
-        TheFacade facade = new TheFacade(Persistence.createEntityManagerFactory("PU"));
-        
-        facade.addPerson("Matyas", "Feher", "fehermatyas@gmail.com");
-        facade.addPerson("Mark", "Herczegfalvy", "herczegfalvy@gmail.com");
-        facade.addPerson("Balint", "Gergely", "gergely.balint@gmail.com");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu");
+        Address adress = new Address("Random Street.", "It's an amazing place to not live in");
+        CityInfo city = new CityInfo(2300, "Copenhagen");
+        Company company = new Company("Balabazorus", "Poop","asdasdasd@asdasda.com", 10089538, 100, 10);
+        Hobby hobby = new Hobby("Gaming", "Just click buttons on my PC");
+        InfoEntity infoentity = new InfoEntity("imcool@gmail.com");
+        Person person = new Person("Zygi", "Pranka", "gdgsdgf@fdgfd.com");
+        Phone phone = new Phone(50324565, "My Phone");
+        EntityManager em = emf.createEntityManager();
+        try{
+        em.getTransaction().begin();
+        em.persist(adress);
+        em.persist(city);
+        em.persist(company);
+        em.persist(hobby);
+        em.persist(infoentity);
+        em.persist(person);
+        em.persist(phone);
+        em.getTransaction().commit();
+        }
+        finally{
+            em.close();
+        }
     }
-    
 }
