21bdd025e1e819a1cfe55ed19573244079014796
enny25 <fetahond@gmail.com>
2016-10-07 12:07:50 +0200

    Something

---
+++ CA2/src/main/java/tester/SchemaBuilder.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tester;
+
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class SchemaBuilder {
+    public static void main(String[] args) {
+        Persistence.generateSchema("pu", null);
+    }
+    
+}
