41fd8baa9d2ba5494b29776113482174d635a907
Zygi <Zygi@10.50.138.131>
2016-10-09 12:41:55 +0200

    Index page edited v2

--- CA2/src/main/webapp/js/getItem.js
+++ CA2/src/main/webapp/js/getItem.js
@@ -1,7 +1,21 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+$(function(){
+    $("#submitBt").on("click", function () {
+        var searchAttribute = $("@searchItem").val();
+       if($("#option").val() === "1"){
+          
+       }
+       else if($("#option").val() === "2"){
+           
+       }
+       else if($("#option").val() === "3"){
+          
+       }
+       else if($("#option").val() === "4"){
+           
+       }
+       else if($("#option").val() === "5"){
+           
+       }
+   }); 
+});
 
--- CA2/src/main/webapp/js/placeholderValue.js
+++ CA2/src/main/webapp/js/placeholderValue.js
@@ -2,19 +2,19 @@ $(function(){
    
    $("#option").on("click", function () {
        if($(this).val() === "1"){
-           $("#seachItem").attr('placeholder', 'First Name, Last Name');
+           $("#searchItem").attr('placeholder', 'First Name, Last Name');
        }
        else if($(this).val() === "2"){
-           $("#seachItem").attr('placeholder', 'Company Name, CVR, etc.');
+           $("#searchItem").attr('placeholder', 'Company Name, CVR, etc.');
        }
        else if($(this).val() === "3"){
-           $("#seachItem").attr('placeholder', 'Phone Number');
+           $("#searchItem").attr('placeholder', 'Phone Number');
        }
        else if($(this).val() === "4"){
-           $("#seachItem").attr('placeholder', 'Name of the Hobby');
+           $("#searchItem").attr('placeholder', 'Name of the Hobby');
        }
        else if($(this).val() === "5"){
-           $("#seachItem").attr('placeholder', 'Street, Zip Code, City');
+           $("#searchItem").attr('placeholder', 'Street, Zip Code, City');
        }
    });
 });
